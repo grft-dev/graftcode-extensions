ccabae3c01b3892948caeea9c38b93ef2feb9f58
Michał Komor <mkomor@graftcode.com>
2026-09-14 15:51:33 +0200

    add servicebus sample

---
+++ samples/servicebus/ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿graft.nuget.SimpleNetClass.GraftConfig.SetConfig("./config.json");
+
+for (int i = 0; i < 50; i++)
+{
+    var result = graft.nuget.SimpleNetClass.Class1.Multiply(i, i + 1);
+    Console.WriteLine($"The result of multiplying {i} and {i + 1} is: {result}");
+}
---
+++ samples/servicebus/SimpleNetClass/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleNetClass;
+
+public class Class1
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
